5ae16eb28f54a1d89ca12fa0667ab55253b5da31
Mojo <36717552+JodieMM@users.noreply.github.com>
2019-06-18 19:34:15 +1000

    Initial commit of application

---
+++ OptiComposer/OptiComposer/SheetMusic.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace OptiComposer
+{
+    partial class SheetMusic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPanel = new System.Windows.Forms.PictureBox();
+            this.NoteHighlight = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DrawPanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NoteHighlight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawPanel
+            // 
+            this.DrawPanel.BackColor = System.Drawing.Color.White;
+            this.DrawPanel.Location = new System.Drawing.Point(42, 42);
+            this.DrawPanel.Name = "DrawPanel";
+            this.DrawPanel.Size = new System.Drawing.Size(650, 350);
+            this.DrawPanel.TabIndex = 0;
+            this.DrawPanel.TabStop = false;
+            this.DrawPanel.MouseHover += new System.EventHandler(this.DrawPanel_Hover);
+            // 
+            // NoteHighlight
+            // 
+            this.NoteHighlight.BackColor = System.Drawing.Color.Black;
+            this.NoteHighlight.Location = new System.Drawing.Point(12, 119);
+            this.NoteHighlight.Name = "NoteHighlight";
+            this.NoteHighlight.Size = new System.Drawing.Size(55, 74);
+            this.NoteHighlight.TabIndex = 1;
+            this.NoteHighlight.TabStop = false;
+            this.NoteHighlight.Visible = false;
+            this.NoteHighlight.Click += new System.EventHandler(this.NoteHighlight_Click);
+            // 
+            // SheetMusic
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(734, 561);
+            this.Controls.Add(this.NoteHighlight);
+            this.Controls.Add(this.DrawPanel);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "SheetMusic";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OptiComposer";
+            ((System.ComponentModel.ISupportInitialize)(this.DrawPanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NoteHighlight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DrawPanel;
+        private System.Windows.Forms.PictureBox NoteHighlight;
+    }
+}
+
---
+++ OptiComposer/OptiComposer/SheetMusic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OptiComposer
+{
+    /// <summary>
+    /// The form for creating sheet music.
+    /// </summary>
+    public partial class SheetMusic : Form
+    {
+        /// <summary>
+        /// Initialiser for the sheet music form.
+        /// </summary>
+        public SheetMusic()
+        {
+            InitializeComponent();
+            
+            // TODO Draw initial lines
+        }
+
+
+
+        // ----- I/O -----
+
+        /// <summary>
+        /// Moves the highlight based on where the cursor is currently hovering.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DrawPanel_Hover(object sender, EventArgs e)
+        {
+            // TODO Check if mouse is over a relevant section
+            // If so, move and reshape highlight to that section
+        }
+
+        /// <summary>
+        /// Reacts to the click of the highlight based on what is highlighted.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NoteHighlight_Click(object sender, EventArgs e)
+        {
+            // TODO Check if a note has been clicked
+            // If so, mark it as selected
+            // If not, insert a single-beat note at the selected location
+        }
+    }
+}
